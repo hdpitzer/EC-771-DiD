e383f78145fd85c89fa9d72e853b6e81b8461873
Hannah <hpitzer@emory.edu>
2024-02-29 15:48:45 -0500

    Begin cleaning HCRIS

---
+++ DD.R
@@ -0,0 +1,19 @@
+###################################################################################################################################################
+
+## Author:        Hannah Pitzer
+## Date Created:  2/29/2024
+## Date Edited:   2/29/2024
+###################################################################################################################################################
+
+if (!require("pacman")) install.packages("pacman")
+library(pacman)
+pacman::p_load(tidyverse, ggplot2, dplyr, lubridate)
+
+
+setwd("C:/Users/Student/OneDrive - Emory University/02 Spring 2024/Health II/EC-771-DiD")
+
+getwd()
+
+
+
+source('data-code/_HCRIS_Data.R')
